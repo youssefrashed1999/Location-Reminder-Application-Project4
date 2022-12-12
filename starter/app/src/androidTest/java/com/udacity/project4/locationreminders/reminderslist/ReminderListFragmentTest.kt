863fa999f1afdd0f2b34cd8a0c3a3aeb10fe873f
Youssef Rashed <youssef.rashed1999@gmail.com>
2022-12-12 21:43:23 +0200

    Update ReminderListFragmentTest.kt
    adding the tools needed for the test process

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -2,6 +2,7 @@ package com.udacity.project4.locationreminders.reminderslist
 
 import android.content.Context
 import android.os.Bundle
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -12,10 +13,22 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.get
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -24,7 +37,46 @@ import org.mockito.Mockito.verify
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest {
-
+    private lateinit var repository: ReminderDataSource
+    //execute each task synchronously using architecture component
+    @get:Rule
+    private var instantTaskExecutorRule= InstantTaskExecutorRule()
+    @Before
+    fun init(){
+        //stop koin
+        stopKoin()
+        //create koin module
+        val myModule= module {
+            viewModel {
+                RemindersListViewModel(
+                    getApplicationContext(),
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    getApplicationContext(),
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                RemindersLocalRepository(get()) as ReminderDataSource
+            }
+            single {
+                LocalDB.createRemindersDao(getApplicationContext())
+            }
+        }
+        //start koin with the created module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //get real repository
+        repository=get()
+        //clear data
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
