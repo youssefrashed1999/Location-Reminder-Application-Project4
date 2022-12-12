5d9c5ea93c826c9f5dcdacd8e435172f80b59fc2
Youssef Rashed <youssef.rashed1999@gmail.com>
2022-12-12 13:50:37 +0200

    Update AuthenticationActivity.kt
    override onActivityResult() method as if the sign in flow was completed succesfully send the user to the reminder activity

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,9 +1,13 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.project4.R
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -16,10 +20,6 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
-
     }
     //TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
     private fun launchSignInFlow() {
@@ -31,5 +31,20 @@ class AuthenticationActivity : AppCompatActivity() {
             AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
             SIGN_IN_REQUEST_CODE
         )
+    }
+    //TODO: If the user was authenticated, send him to RemindersActivity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== SIGN_IN_REQUEST_CODE){
+            //if the user signed in successfully navigate him to reminders activity
+            if(resultCode== Activity.RESULT_OK){
+                navigate()
+            }
+        }
+    }
+    private fun navigate() {
+        val intent=Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
