e6312c358a578dd605898309dbe4a796692d9e45
Youssef Rashed <youssef.rashed1999@gmail.com>
2022-12-12 17:17:14 +0200

    Update FakeDataSource.kt
    adding the fake data source class to act as a double for the real data source

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,38 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders:MutableList<ReminderDTO>?= mutableListOf()) : ReminderDataSource{
 
 //    TODO: Create a fake data source to act as a double to the real data source
-
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+private var shouldReturnError=false
+    fun setReturnsError(value:Boolean){
+        shouldReturnError=value
     }
-
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
-
-    override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
-    }
-
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if(shouldReturnError){
+            return Result.Error("Error has occurred")
+        }
+        reminders?.let { return Result.Success(ArrayList(it)) }
+        return Result.Error("Error has occurred")
+    }
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        if(shouldReturnError)
+            return Result.Error("Error has occurred")
+        val reminder=reminders?.find {
+            it.id==id
+        }
+        if(reminder!=null)
+            return Result.Success(reminder)
+        else
+            return Result.Error("No reminder with this ID")
+    }
+    
 
 
 }
