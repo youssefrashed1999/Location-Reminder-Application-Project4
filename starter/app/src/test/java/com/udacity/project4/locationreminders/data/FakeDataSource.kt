dd845e504d2d58827720b702b65ad04ea6408723
Youssef Rashed <youssef.rashed1999@gmail.com>
2022-12-14 12:12:27 +0200

    Update FakeDataSource.kt
    the data that the fake data source holds should be declared as non-nullable.

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,7 +4,7 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var reminders:MutableList<ReminderDTO>?= mutableListOf()) : ReminderDataSource{
+class FakeDataSource(var reminders:MutableList<ReminderDTO> = mutableListOf()) : ReminderDataSource{
 
 //    TODO: Create a fake data source to act as a double to the real data source
 private var shouldReturnError=false
