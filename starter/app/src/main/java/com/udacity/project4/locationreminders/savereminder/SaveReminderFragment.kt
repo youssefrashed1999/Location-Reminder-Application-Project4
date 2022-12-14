32ca560b16f16840fd2c0a54ea48df32330ac9df
Youssef Rashed <youssef.rashed1999@gmail.com>
2022-12-14 12:44:33 +0200

    Fixing issues with flags in PendingIntent
    adding the flags corresponding to api S as required in the review

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -43,10 +43,14 @@ class SaveReminderFragment : BaseFragment() {
     private lateinit var reminder:ReminderDataItem
     //check what api the device is running
     private val runningQOrLater = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    val flags= if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S){
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+    }else{PendingIntent.FLAG_UPDATE_CURRENT}
+
     private val geoFencePendingIntent: PendingIntent by lazy {
         val intent= Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
         intent.action= ACTION_GEOFENCE_EVENT
-        PendingIntent.getBroadcast(requireContext(),0,intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        PendingIntent.getBroadcast(requireContext(),0,intent,flags)
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
